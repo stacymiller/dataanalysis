b8540de564cac48c05097c6919b47c8a25033678
Anastasia Miller <anastasia.miller@jetbrains.com>
2015-09-19 23:04:41 +0300

    summer updates

--- cfg.py
+++
@@ -1,6 +0,0 @@
-c = get_config()
-c.Application.verbose_crash=True
-c.NbConvertApp.export_format = 'pdf'
-# c.NbConvertApp.postprocessor_class = 'pdf'
-
-c.Exporter.template_file = 'russian-article.tplx'
